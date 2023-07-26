6c607bff4144d128eb5f3cf4d5841bc5b3a36aec
Digiblade <ak.chourasia.007@gmail.com>
2023-07-26 22:58:16 +0530

    comment crm db

--- Routers/routingEngine.js
+++ Routers/routingEngine.js
@@ -1,25 +1,25 @@
-let express = require("express");
-let router = express.Router();
-let {
-  getCrmData,
-  insertCrmData,
-  updateCrmData,
-} = require("./routingEngine.helper");
-let { crmConnection } = require("../DBConfig/dbConfig");
-let crmSession = crmConnection();
+// let express = require("express");
+// let router = express.Router();
+// let {
+//   getCrmData,
+//   insertCrmData,
+//   updateCrmData,
+// } = require("./routingEngine.helper");
+// // let { crmConnection } = require("../DBConfig/dbConfig");
+// // let crmSession = crmConnection();
 
-// CRUD METHODS FOR neo4j
-router.get("/getCrmData/:label/:id?", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  getCrmData(req, res, crmSession);
-});
-router.post("/insertCrmData/:label", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  insertCrmData(req, res, crmSession);
-});
-router.put("/updateCrmData/:label/:id", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  updateCrmData(req, res, crmSession);
-});
+// // CRUD METHODS FOR neo4j
+// // router.get("/getCrmData/:label/:id?", (req, res) => {
+// //   res.setHeader("Content-Type", "application/json");
+// //   getCrmData(req, res, crmSession);
+// // });
+// // router.post("/insertCrmData/:label", (req, res) => {
+// //   res.setHeader("Content-Type", "application/json");
+// //   insertCrmData(req, res, crmSession);
+// // });
+// // router.put("/updateCrmData/:label/:id", (req, res) => {
+// //   res.setHeader("Content-Type", "application/json");
+// //   updateCrmData(req, res, crmSession);
+// // });
 
-module.exports = router;
+// module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let app = express();
-let routes = require("./Routers/routingEngine");
+// let routes = require("./Routers/routingEngine");
 let { jwtConnection, mysqlClient } = require("./DBConfig/dbConfig");
 let { register, login, mySqlRegister } = require("./AuthHelpers/auth.helper");
 let auth = require("./Middleware/auth");
@@ -36,21 +36,23 @@ app.post("/auth/login", (req, res) => {
   login(req, res, jwtSession);
 });
 
+
+
+//mongo apis
+
+
+
+
+
+
+
+
+
 // crm apis
-app.use("/api", auth, routes);
+// app.use("/api", auth, routes);
 
 app.post("/const/", (req, res) => {
-  let constData = {
-    CRM_DATABASE: "CRM db",
-    CRM_USER: "neo4j",
-    CRM_PASSWORD: "df8SzGQjdF8VAA9YUcEbh6pydif5QpShU3nrH6l8g8I",
-    CRM_CONNECTION: "neo4j+s://92c11f48.databases.neo4j.io",
-    JWT_KEY: "ttcF8Lap6ItTHXJWvtrSpodYtCRGRD3u",
-    JWT_DATABASE: "JWT db",
-    JWT_USER: "neo4j",
-    JWT_PASSWORD: "1qaz!QAZ",
-    JWT_CONNECTION: "neo4j+s://d2de0c61.databases.neo4j.io",
-  };
+  let constData = { item: { ...process.env } };
   res.status(500).send(constData);
 });
 
